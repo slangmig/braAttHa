86530ce9bd7b0080f91d6cef823ea9fee6111f53
slangmig <26557197+slangmig@users.noreply.github.com>
2023-09-15 22:08:07 +0200

    Create bootstrap.sh

---
+++ vagrant/multinodes/bootstrap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Basics stuff when creating a new host
+
+apt-get update -qq
+apt-get upgrade -qq --yes --no-show-upgraded
+apt-get autoremove -qq --purge --yes
+apt-get clean -qq
+
+# Disable SWAP
+swapoff -a
+sed -i.bkp '/swap/ s/^\(.*\)$/#\1/g' /etc/fstab
+
+# Remove swap-file if it exist
+SWAPFILE=$(ls -1 / | grep 'swap.img')
+if [ -f "/$SWAPFILE" ]; then
+  rm -f "/$SWAPFILE"
+fi
+
+# Install SR CA trust certificates
+mkdir -p /usr/local/share/ca-certificates/sverigesradio
+curl -SsfLk -o /usr/local/share/ca-certificates/corp-cert/CA_Root.crt 'https://some-url'
+curl -SsfLk -o /usr/local/share/ca-certificates/corp-cert/CA_Issuing.crt 'https://some-url'
+update-ca-certificates
